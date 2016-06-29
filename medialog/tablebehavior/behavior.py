b696ecb33b24b20cc60cf7b434db249526f26e11
Espen Moe-Nilssen <espenmoe-nilssen@Espen.lan>
2016-06-29 21:45:44 +0200

    list

--- medialog/tablebehavior/behavior.py
+++ medialog/tablebehavior/behavior.py
@@ -1,18 +1,22 @@
 from zope import schema
+from zope.interface import implements
 #from plone.directives import dexterity
 from plone.directives import form
 from plone.autoform.interfaces import IFormFieldProvider
 from zope.interface import alsoProvides
 from zope.i18nmessageid import MessageFactory
+from zope.schema.interfaces import IFromUnicode
 
 from medialog.tablebehavior.widgets.widget import TableFieldWidget
 
 _ = MessageFactory('medialog.tablebehavior')
+from zope.interface import implementer
 
-
+@implementer(IFromUnicode)
 class ITableBehavior(form.Schema):
     """ A field for a table (to and from)"""
-    
+
+       
     form.fieldset(
         'plotly',
         label = 'Table',
@@ -21,9 +25,9 @@ class ITableBehavior(form.Schema):
         ],
      )  
     
-    table = schema.Text(
+    table = schema.List(
         title=u'Table',
-        default=u'[["A", "B"], ["A1", "B1"]]',
+        default=[["A", "B"], ["A1", "B1"]],
         required=False,
     )  
     
